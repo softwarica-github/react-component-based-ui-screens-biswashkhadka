bb89fbd702c86080934a380ca18ad146cadcea2c
biswashkhadka <biswash005@gmail.com>
2020-01-24 08:45:23 +0545

    ui design

---
+++ src/components/FixedPlugin/FixedPlugin.jsx
@@ -0,0 +1,53 @@
+/*!
+
+=========================================================
+* Light Bootstrap Dashboard React - v1.3.0
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/light-bootstrap-dashboard-react
+* Copyright 2019 Creative Tim (https://www.creative-tim.com)
+* Licensed under MIT (https://github.com/creativetimofficial/light-bootstrap-dashboard-react/blob/master/LICENSE.md)
+
+* Coded by Creative Tim
+
+=========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+*/
+/*eslint-disable*/
+import React, { Component } from "react";
+import Toggle from "react-toggle";
+
+import imagine1 from "assets/img/sidebar-1.jpg";
+import imagine2 from "assets/img/sidebar-2.jpg";
+import imagine3 from "assets/img/sidebar-3.jpg";
+import imagine4 from "assets/img/sidebar-4.jpg";
+
+class FixedPlugin extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      classes: "dropdown show-dropdown open",
+      bg_checked: true,
+      bgImage: this.props.bgImage
+    };
+  }
+  handleClick = () => {
+    this.props.handleFixedClick();
+  };
+  onChangeClick = () => {
+    this.props.handleHasImage(!this.state.bg_checked);
+    this.setState({ bg_checked: !this.state.bg_checked });
+  };
+  render() {
+    return (
+      <div className="fixed-plugin">
+        <div id="fixedPluginClasses" className={this.props.fixedClasses}>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default FixedPlugin;
